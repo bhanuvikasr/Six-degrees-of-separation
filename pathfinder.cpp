8f691ddac4d616cc58d8bd590bd9df0b6cde5ae2
Dion <dion.chung@brightscope.com>
2016-11-11 16:05:29 -0800

    initial files

---
+++ pathfinder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char**argv){
+
+  string movie_casts = argv[1];
+  string edge_weight = argv[2];
+  string find_pairs = argv[3];
+  string output = argv[4];
+
+}
