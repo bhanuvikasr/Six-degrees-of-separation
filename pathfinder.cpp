8e0b930f361fd5560464fcac724b7f50290dcb05
bhanuvikasr <bhanuvikasr@gmail.com>
2016-11-16 20:24:22 -0800

    added logic to read actor pairs from a file

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -47,7 +47,7 @@ void outputPath(ActorNode* actor, ofstream & out) {
   string temp;
   while (actor->previous.second) {
     temp = "(" + actor->previous.second->name + ")--[" + actor->previous.first->name + "#@" + to_string(2016 - actor->previous.first->weight) + "]-->";
-    cout << actor->previous.first->name << " and " << actor->previous.second->name << " at distance " << actor->years << endl;
+    // cout << actor->previous.first->name << " and " << actor->previous.second->name << " at distance " << actor->years << endl;
     path = temp + path;
     actor = actor->previous.second;
   }
@@ -59,7 +59,6 @@ void reset(ActorGraph& G) {
     it->second->years = -1;
     it->second->previous = {NULL, NULL};
   }
-
 }
 
 int main(int argc, char**argv){
@@ -82,15 +81,59 @@ int main(int argc, char**argv){
   ActorGraph G;
   G.loadFromFile(movie_casts, isUnweighted);
 
-  string name1 = "50 CENT";
-  ActorNode* actor1 = G.actors_map[name1];
-  string name2 = "ABKARIAN, SIMON";
-  ActorNode* actor2 = G.actors_map[name2];
-
   out << "(actor)--[movie#@year]-->(actor)--...\n";
-  BFS(actor1, actor2);
-  outputPath(actor2, out);
-  reset(G);
+  // for each line in the find_pairs
+  bool have_header = false;
+
+  while (in) {
+      string s;
+
+      // get the next line
+      if (!getline( in, s )) break;
+
+      if (!have_header) {
+          // skip the header
+          have_header = true;
+          continue;
+      }
+      istringstream ss( s );
+      vector <string> record;
+
+      while (ss) {
+          string next;
+
+          // get the next string before hitting a tab character and put it in 'next'
+          if (!getline( ss, next, '\t' )) break;
+
+          record.push_back( next );
+      }
+
+      if (record.size() != 2) {
+          // we should have exactly 2 columns
+          continue;
+      }
+      string actor1_name(record[0]);
+      string actor2_name(record[1]);
+
+      // we have actor1 and actor2, now what?
+      ActorNode* actor1 = G.actors_map.at(actor1_name);
+      ActorNode* actor2 = G.actors_map.at(actor2_name);
+      // if(actor1 && actor2 != NULL){
+        BFS(actor1, actor2);
+        outputPath(actor2, out);
+        reset(G);
+      // }
+      // else{
+      //   std::cout << "Hello there you got a problem" << std::endl;
+      // }
+
+  }
+
+  if (!in.eof()) {
+      cerr << "Failed to read " << find_pairs << "!\n";
+      return false;
+  }
+
 
   in.close();
   out.close();
